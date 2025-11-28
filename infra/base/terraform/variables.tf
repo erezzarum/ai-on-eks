7f6623040102b2e413773cf80bed8f390cb381f2
Erez Zarum <erezz@amazon.com>
2025-11-28 14:55:51 +0200

    Update KubeRay operator to latest version 1.5.1

--- infra/base/terraform/variables.tf
+++ infra/base/terraform/variables.tf
@@ -201,6 +201,11 @@ variable "enable_kuberay_operator" {
   type        = bool
   default     = false
 }
+variable "kuberay_operator_version" {
+  description = "KubeRay operator default version"
+  type        = string
+  default     = "1.5.1"
+}
 variable "huggingface_token" {
   description = "Hugging Face Secret Token"
   type        = string
